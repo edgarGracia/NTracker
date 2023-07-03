ad1fd74b58f068f1f528e11b31b9ec7844ddf23e
Edgar <edgarGrac@gmail.com>
2023-07-03 20:12:22 +0200

    Tasks

--- NTracker/main.py
+++ NTracker/main.py
@@ -1,9 +1,12 @@
+import logging
 
 import hydra
+from hydra.utils import instantiate
 from omegaconf import DictConfig
 
 from NTracker.tasks.track import Track
 
+logger = logging.getLogger(__name__)
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig):
@@ -12,9 +15,10 @@ def main(cfg: DictConfig):
     Args:
         cfg (DictConfig): A configuration object.
     """
-    track = Track(cfg)
-    track.run()
-    
+    for task_params in cfg.tasks:
+        logger.info(f"Running task: {task_params._target_}")
+        task = instantiate(task_params, cfg=cfg, _recursive_=False)
+        task.run()
 
 
 if __name__ == "__main__":
